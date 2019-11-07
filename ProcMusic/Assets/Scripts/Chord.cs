2ea08ca70685a152edeefbdcb377b8f5939750dd
Unknown <jacattelona@wpi.edu>
2019-11-07 14:24:42 -0500

    Chord

---
+++ ProcMusic/Assets/Scripts/Chord.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chord : MonoBehaviour
+{
+    const float TOP = 5.5f;
+    const float BOTTOM = -3.5f;
+    const float LEFT = -7.5f;
+    const float RIGHT = 7.5f;
+
+
+    public AudioClip[] notes;
+    public GameObject particle;
+
+    AudioSource source;
+    float counter = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter -= Time.deltaTime;
+        if (counter < 0)
+        {
+            counter = 5f;
+        }
+    }
+
+
+
+    void PlayNote(int index)
+    {
+        //source.PlayOneShot(notes[index]);
+        GameObject part = Instantiate(particle);
+        part.transform.position = new Vector3(Random.Range(LEFT, RIGHT), Random.Range(BOTTOM, TOP), 0);
+    }
+
+}
