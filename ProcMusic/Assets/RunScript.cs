e177157542e739ecdf06a3d7bd74dbbec96e0c7e
Grant Ferguson <gferguson@wpi.edu>
2019-11-10 16:16:11 -0500

    It me stuff

---
+++ ProcMusic/Assets/RunScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunScript : MonoBehaviour
+{
+    public Manager manage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //generate the manager object and list
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Generate the chords
+
+        //generate the effects
+
+        //generate background effect
+
+
+        //display effects
+    }
+}
